3b10d28392a44f354db021963c162c9eb1637e22
Andre Taulien <a.taulien@live.de>
2016-01-14 15:36:16 +0100

    Initializing pointers to nullptr on engine startup

--- RenderingAPI/REngine.cpp
+++ RenderingAPI/REngine.cpp
@@ -6,6 +6,29 @@
 #include "RThreadPool.h"
 #include <thread>
 
+/** Simple struct to be initialized right after the program was loaded.
+	Initializes these engine-resources to a defined value. */
+struct _hlpObject
+{
+	// Perform some startup-code
+	_hlpObject()
+	{
+		REngine::RenderingDevice = nullptr;
+		REngine::ResourceCache = nullptr;
+		REngine::DynamicBufferCache = nullptr;
+		REngine::ThreadPool = nullptr;
+	}
+
+	~_hlpObject()
+	{
+		// Clean exit?
+		assert(!REngine::RenderingDevice);
+		assert(!REngine::ResourceCache);
+		assert(!REngine::DynamicBufferCache);
+		assert(!REngine::ThreadPool);
+	}
+}__hlpObj;
+
 /**
  * Initializes these objects 
  */
@@ -26,4 +49,10 @@ void REngine::UninitializeEngine()
 	delete REngine::ResourceCache;
 	delete REngine::RenderingDevice;
 	delete REngine::DynamicBufferCache;
+	delete REngine::ThreadPool;
+
+	REngine::RenderingDevice = nullptr;
+	REngine::ResourceCache = nullptr;
+	REngine::DynamicBufferCache = nullptr;
+	REngine::ThreadPool = nullptr;
 }
