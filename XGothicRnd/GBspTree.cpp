746e44cd6029e3069994e0eb97e841cf97044d67
Andre Taulien <a.taulien@live.de>
2016-01-18 23:49:37 +0100

    Added better devided quad-tree as helper to draw the worldmesh

--- XGothicRnd/GBspTree.cpp
+++ XGothicRnd/GBspTree.cpp
@@ -6,11 +6,17 @@
 #include <RBuffer.h>
 #include <RBufferCollection.h>
 #include "zCCamera.h"
+#include "GMaterial.h"
+#include "GTexture.h"
+#include "GMeshIndexed.h"
+
+// Number of subdivides the quadtree should do
+const int QUAD_TREE_NUM_SUBLEVELS = 3;
 
 GBspTree::GBspTree(zCBspTree* sourceTree) : GzObjectExtension<zCBspTree, GBspTree>(sourceTree)
 {
 	m_SourceTree = sourceTree;
-
+	
 	// Pre-allocate the memory for the nodes
 	m_TreeNodes.resize(sourceTree->GetNumNodes());
 	m_TreeNodes.clear(); // This keeps the memory allocated
@@ -21,11 +27,11 @@ GBspTree::GBspTree(zCBspTree* sourceTree) : GzObjectExtension<zCBspTree, GBspTre
 	std::vector<ExTVertexStruct> vertices;
 	std::vector<unsigned int> indices;
 	std::vector<ExTVertexStruct> indicedVertices;
-
+	std::vector<zCPolygon*> trianglePolys;
 
 	LogInfo() << "Building node triangle-lists";
 	// Generate vertex-lists for each node
-	m_RootNode->BuildTriangleList(vertices);
+	m_RootNode->BuildTriangleList(vertices, trianglePolys);
 
 	LogInfo() << "Indexing worldmesh...";
 	// Create indexed mesh
@@ -45,9 +51,16 @@ GBspTree::GBspTree(zCBspTree* sourceTree) : GzObjectExtension<zCBspTree, GBspTre
 		ECPUAccessFlags::CA_NONE,
 		"WorldMesh");
 
-	LogInfo() << "Generating indexded meshes for BSP-Nodes...";
+	//LogInfo() << "Generating indexded meshes for BSP-Nodes...";
 	// Generate indices for each node
-	m_RootNode->GenerateIndexedMesh(indices, m_WorldMeshBuffer, m_WorldIndexBuffer);
+	//m_RootNode->GenerateIndexedMesh(indices, m_WorldMeshBuffer, m_WorldIndexBuffer);
+
+	LogInfo() << "Generating Quad-Tree...";
+	m_QuadTree = new GQuadTree<QuadTreeNode>(nullptr, m_RootNode->GetBBox());
+	m_QuadTree->Subdivde(QUAD_TREE_NUM_SUBLEVELS);
+
+	LogInfo() << "Splitting World-Mesh...";
+	BuildQuadTreeVertexData(indicedVertices, indices, trianglePolys);
 
 	LogInfo() << "Constructing collected indexbuffer...";
 	m_WorldIndexBuffer->Construct(EBindFlags::B_INDEXBUFFER);
@@ -58,6 +71,7 @@ GBspTree::~GBspTree(void)
 {
 	REngine::ResourceCache->DeleteResource(m_WorldMeshBuffer);
 	REngine::ResourceCache->DeleteResource(m_WorldIndexBuffer);
+	delete m_QuadTree;
 }
 
 /**
@@ -73,22 +87,134 @@ GBspNode* GBspTree::AddBspNode(zCBspBase* source)
 	return node;
 }
 
+/** Generates the quad-trees vertex data */
+void GBspTree::BuildQuadTreeVertexData(const std::vector<ExTVertexStruct>& vertices, const std::vector<unsigned int>& indices, const std::vector<zCPolygon*>& trianglePolys)
+{
+	// First split the indices into the nodes
+	for(int i=0;i<indices.size();i+=3)
+	{
+		auto tuple = std::make_tuple(vertices[indices[i]].Position, vertices[indices[i+1]].Position, vertices[indices[i+2]].Position);
+
+		// Add indices to the nodes on success
+		std::function<void(GQuadTree<QuadTreeNode>*)> suc = [&](GQuadTree<QuadTreeNode>* n)
+		{
+			zCPolygon* poly = trianglePolys[i / 3];
+			WorldMeshPart& p = n->GetData().m_MeshParts[std::make_pair(poly->GetLightmap(), poly->GetMaterial())];
+
+			// Add indices to mesh-part
+			p.m_Indices.push_back(indices[i]);
+			p.m_Indices.push_back(indices[i+1]);
+			p.m_Indices.push_back(indices[i+2]);
+		};
+
+		m_QuadTree->AddTriangle(tuple, suc);
+	}
+
+	// Then, generate their pipeline-states and buffer portions
+	std::function<void(GQuadTree<QuadTreeNode>* n)> gen = [&](GQuadTree<QuadTreeNode>* n)
+	{
+		auto& map = n->GetData().m_MeshParts;
+
+		for(auto& pair : map)
+		{
+			WorldMeshPart& part = pair.second;
+
+			// Get extension-objects...
+			part.m_Material = GMaterial::GetFromSource(pair.first.second);
+
+			if(pair.first.first)
+				part.m_Lightmap = GTexture::GetFromSource(pair.first.first->GetTexture());
+
+			// Add data to global index buffer
+			unsigned int start = m_WorldIndexBuffer->AddData(&part.m_Indices[0], part.m_Indices.size());
+
+			// Build buffer portion
+			pair.second.m_Mesh = new GMeshIndexed(m_WorldMeshBuffer,
+				m_WorldIndexBuffer->GetBuffer(),
+				part.m_Indices.size(),
+				0,
+				start);
+			
+			// Create actual pipeline state
+			UpdateMeshPartPipelineState(part);
+		}
+	};
+
+	m_QuadTree->MapTree(gen);
+}
+
+/** Updates the pipeline-state of one meshpart */
+void GBspTree::UpdateMeshPartPipelineState(WorldMeshPart& part)
+{
+	// Delete old state, in case this is a recreation
+	REngine::ResourceCache->DeleteResource(part.m_PipelineState);
+
+	// Get base-state for worldmeshes. This contains objects like the needed shaders.
+	RPipelineState* defState = REngine::ResourceCache->GetCachedObject<RPipelineState>(GConstants::PipelineStates::BPS_WORLDMESH);
+	RStateMachine& sm = REngine::RenderingDevice->GetStateMachine();
+
+	// Assign default values
+	sm.SetFromPipelineState(defState);
+
+	// Now ours...
+	GMeshIndexed* msh = part.m_Mesh;
+	sm.SetVertexBuffer(0, msh->GetMeshVertexBuffer());
+	sm.SetIndexBuffer(msh->GetMeshIndexBuffer());
+
+	if (part.m_Material->GetDiffuse())
+	{
+		UINT mFlags = MPS_NONE;
+
+		sm.SetTexture(0, part.m_Material->GetDiffuse()->GetTexture(), EShaderType::ST_PIXEL);
+
+		// Set lightmap if we have one
+		if(part.m_Lightmap)
+		{
+			part.m_Lightmap->CacheIn(false);
+			sm.SetTexture(1, part.m_Lightmap->GetTexture(), EShaderType::ST_PIXEL);
+
+			mFlags |= MPS_LIGHTMAPPED;
+		}
+
+		// Force a cache-in because we need to know if the texture uses alpha-testing
+		// TODO: Maybe this is possible with threading. But is it even worth it?
+		part.m_Material->GetDiffuse()->CacheIn(false);
+
+
+		//if (part.m_Material->GetDiffuse()->GetSourceObject()->GetTextureFlags().HasAlpha)
+		//	LogInfo() << "Alphatest on: " << part.m_Material->GetDiffuse()->GetSourceObject()->GetObjectName();
+
+		// Get the right pixelshader for the mesh-part
+		RPixelShader* ps = part.m_Material->GetMaterialPixelShader(GConstants::RS_WORLD, mFlags);
+		sm.SetPixelShader(ps);
+	}
+
+
+
+	// Construct drawcall
+	part.m_PipelineState = sm.MakeDrawCallIndexed(msh->GetNumIndices(), msh->GetMeshIndexStart(), 0);
+}
+
 /**
  * Renders everyting inside this BSP-Tree from the current frustum
  */
 void GBspTree::Draw(RRenderQueueID queue, std::vector<GVobObject*>& visibleVobs, float objectFarplane)
 {
 	// Extract information about the camera so we have it all on stack. Better cache locality.
-	GBspNode::BSPRenderInfo info;
+	BSPRenderInfo info;
 	info.CameraDirection = zCCamera::GetActiveCamera()->GetCameraDirection();
 	info.CameraPostion = zCCamera::GetActiveCameraPosition();
 	memcpy(info.FrustumPlanes, zCCamera::GetActiveCamera()->GetFrustumPlanes(), sizeof(info.FrustumPlanes));
 	memcpy(info.FrustumSignBits, zCCamera::GetActiveCamera()->GetFrustumSignBits(), sizeof(info.FrustumSignBits));
 	info.ClipFlags = CLIP_FLAGS_FULL;
 	info.ObjectFarplane = objectFarplane;
+	info.WorldMaxY = m_RootNode->GetBBox().m_Max.y;
 
 	float minNodeSizeXZ = 8000.0f;
 	m_RootNode->DrawNodeRecursive(minNodeSizeXZ, queue, info, visibleVobs);
+
+	// Draw world-mesh
+	DrawQuadTreeNodes(info, queue);
 
 	// DEBUG
 	zCBspSector* startSector = m_SourceTree->GetCurrentCameraSector();
@@ -102,4 +228,82 @@ void GBspTree::Draw(RRenderQueueID queue, std::vector<GVobObject*>& visibleVobs,
 
 	Engine::Game->AddFrameDebugLine(std::to_string(visibleVobs.size()) + " Vobs drawn");
 
+}
+
+/** Draws a world-mesh-part */
+void GBspTree::DrawWorldMeshPart(WorldMeshPart& part, RRenderQueueID queue)
+{
+	// Make sure we have textures
+	if(part.m_Material->CacheTextures())
+	{
+		// Make sure the lightmap is loaded
+		if(part.m_Lightmap)
+			part.m_Lightmap->CacheIn(false);
+
+		// Check if the state is valid and the texture still the same
+		if(!part.m_PipelineState 
+			|| part.m_PipelineState->IDs.MainTexture != part.m_Material->GetDiffuse()->GetTexture()->GetID())
+		{
+			// Assign new state
+			UpdateMeshPartPipelineState(part);
+		}
+
+		if(part.m_PipelineState) // Catch failed states
+		{
+			REngine::RenderingDevice->QueuePipelineState(part.m_PipelineState, queue);
+		}
+	}
+}
+
+/** Draws the pipeline-states from the visible quad-tree nodes */
+void GBspTree::DrawQuadTreeNodes(BSPRenderInfo& info, RRenderQueueID queue)
+{
+	info.ClipFlags = CLIP_FLAGS_FULL;
+
+	// Lambda to traverse to the leafs and draw them
+	std::function<void(GQuadTree<QuadTreeNode>*)> fnDraw = [&](GQuadTree<QuadTreeNode>* n)
+	{
+		if(n->IsLeaf())
+		{
+			// Draw all parts of this node
+			for(auto& pair : n->GetData().m_MeshParts)
+			{
+				DrawWorldMeshPart(pair.second, queue);
+			}
+		}
+		else
+		{
+			// Go deeper...
+			n->WalkSubs(fnDraw);
+		}
+	};
+
+	// Lambda to traverse the tree and clip invisible nodes
+	std::function<void(GQuadTree<QuadTreeNode>*)> fnClip = [&](GQuadTree<QuadTreeNode>* n)
+	{
+		// Check if this node is inside the bounding frustum
+		zTCam_ClipType clip = zCCamera::BBox3DInFrustumCached(n->GetBBox(), info.FrustumPlanes, info.FrustumSignBits, n->GetData().m_FrustumTestCache, info.ClipFlags);
+
+		RTools::LineRenderer.AddAABBMinMax(n->GetBBox().m_Min, n->GetBBox().m_Max, float4(1,0,0,1));
+
+		if(clip == ZTCAM_CLIPTYPE_OUT)
+			return; // Don't go futher into the tree, entire node is invisible
+
+		
+		if(clip == ZTCAM_CLIPTYPE_IN || n->IsLeaf())
+			n->WalkTree(fnDraw); // Draw everything in here and below, if the full node is inside or we are at the bottom
+		else
+			n->WalkSubs(fnClip); // If only crossing, go deeper and try to clip more nodes
+	};
+
+	std::function<void(GQuadTree<QuadTreeNode>*)> fnDebug = [&](GQuadTree<QuadTreeNode>* n)
+	{
+		RTools::LineRenderer.AddAABBMinMax(n->GetBBox().m_Min, n->GetBBox().m_Max, float4(1,0,0,1));
+
+		n->WalkSubs(fnDebug);
+	};
+
+	//m_QuadTree->WalkTree(fnDraw);
+	m_QuadTree->WalkTree(fnDebug);
+	m_QuadTree->WalkTree(fnClip);
 }
--- XGothicRnd/GBspTree.h
+++ XGothicRnd/GBspTree.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "GzObjectExtension.h"
+#include "GQuadTree.h"
+#include "GMeshIndexed.h"
+#include "VertexTypes.h"
 
 /**
  * Additional data for the usual zCBspTree
@@ -8,12 +11,67 @@ class zCBspTree;
 struct zCBspBase;
 class GBspNode;
 class GVobObject;
+class GTexture;
+class GMaterial;
+struct RPipelineState;
+class zCLightmap;
+class zCMaterial;
+class zCPolygon;
 
 template<typename T>
 class RBufferCollection;
 class GBspTree : public GzObjectExtension<zCBspTree, GBspTree>
 {
 public:
+	struct BSPRenderInfo
+	{
+		float3 CameraPostion;
+		float3 CameraDirection;
+		zTPlane FrustumPlanes[6];
+		byte FrustumSignBits[6];
+		int ClipFlags;
+		float ObjectFarplane;
+		float WorldMaxY;
+	};
+
+	struct WorldMeshPart
+	{
+		WorldMeshPart()
+		{
+			m_Mesh = nullptr;
+			m_Material = nullptr;
+			m_PipelineState = nullptr;
+			m_Lightmap = nullptr;
+		}
+
+		~WorldMeshPart()
+		{
+			// Delete allocated memory from part
+			delete m_Mesh;
+			REngine::ResourceCache->DeleteResource(m_PipelineState);		
+		}
+
+		GMeshIndexed* m_Mesh;
+		GMaterial* m_Material;
+		GTexture* m_Lightmap;
+		RPipelineState* m_PipelineState;
+
+		// Indices of the full mesh
+		std::vector<unsigned int> m_Indices;
+	};
+
+	// Single node of the worldmesh-quadtree
+	struct QuadTreeNode
+	{
+		QuadTreeNode()
+		{
+			m_FrustumTestCache = -1;
+		}
+
+		std::map<std::pair<zCLightmap*, zCMaterial*>, WorldMeshPart> m_MeshParts;
+		int m_FrustumTestCache;
+	};
+
 	GBspTree(zCBspTree* sourceTree);
 	~GBspTree(void);
 
@@ -34,6 +92,21 @@ public:
 	void SetContainedWorld(GWorld* world){m_ContainedWorld = world;}
 
 private:
+	/** Draws the pipeline-states from the visible quad-tree nodes (The world mesh) */
+	void DrawQuadTreeNodes(BSPRenderInfo& info, RRenderQueueID queue);
+
+	/** Generates the quad-trees vertex data */
+	void BuildQuadTreeVertexData(const std::vector<ExTVertexStruct>& vertices, const std::vector<unsigned int>& indices, const std::vector<zCPolygon*>& trianglePolys);
+	
+	/** Updates the pipeline-state of one meshpart */
+	void UpdateMeshPartPipelineState(WorldMeshPart& part);
+
+	/** Draws a world-mesh-part */
+	void DrawWorldMeshPart(WorldMeshPart& part, RRenderQueueID queue);
+
+	// My own quad-tree for rendering the game.
+	// Gothics own BSP-Tree is too much of a mess.
+	GQuadTree<QuadTreeNode>* m_QuadTree;
 
 	// Root-node
 	GBspNode* m_RootNode;
