b4b9b788f7cf01264545ddacf7186afcc5e8cc44
Andre Taulien <a.taulien@live.de>
2016-01-20 21:15:37 +0100

    Added map for lightmap atlases

--- XGothicRnd/GMainResources.cpp
+++ XGothicRnd/GMainResources.cpp
@@ -11,6 +11,7 @@
 #include "zCMorphMesh.h"
 #include "GMorphMesh.h"
 #include "GParticleFXVisual.h"
+#include <RTextureAtlas.h>
 
 const int DEFAULT_INSTANCE_BUFFER_SIZE = 1024 * sizeof(VobInstanceInfo);
 
@@ -34,6 +35,41 @@ GMainResources::GMainResources(void) :
 GMainResources::~GMainResources(void)
 {
 	REngine::ResourceCache->DeleteResource(m_VobInstanceBuffer);
+}
+
+RTextureAtlas* GMainResources::GetLightmapAtlas(const INT2& singleTextureSize)
+{
+	GASSERT(singleTextureSize.x == singleTextureSize.y, "Lightmaps must have size x == y");
+
+	auto p = std::make_pair(singleTextureSize.x, singleTextureSize.y);
+	auto it = m_LightmapAtlasCache.find(p);
+	if(it == m_LightmapAtlasCache.end())
+	{
+		// Create new atlas
+		m_LightmapAtlasCache[p] = REngine::ResourceCache->CreateResource<RTextureAtlas>();
+	}
+
+	return m_LightmapAtlasCache[p];
+}
+
+// Constructs all lightmap atlases
+void GMainResources::ConstructLightmapAtlases()
+{
+	for(auto p : m_LightmapAtlasCache)
+	{
+		p.second->Construct(INT2(p.first.first, p.first.second));
+		p.second->GetTexture()->SaveToFileAPI("atlas_" + std::to_string(p.first.first) + "x" + std::to_string(p.first.second) + ".png");
+	}
+}
+
+// Deletes all lightmap atlases
+void GMainResources::ClearLightmapAtlases()
+{
+	for(auto p : m_LightmapAtlasCache)
+	{
+		REngine::ResourceCache->DeleteResource(p.second);
+	}
+	m_LightmapAtlasCache.clear();
 }
 
 /** Creates the right type of visual from the source */
--- XGothicRnd/GMainResources.h
+++ XGothicRnd/GMainResources.h
@@ -7,6 +7,7 @@
  */
 
 class GVisual;
+class RTextureAtlas;
 class zCVisual;
 class GMainResources
 {
@@ -23,6 +24,15 @@ public:
 	RPagedBuffer<unsigned int>* GetExSkelPagedIndexBuffer() { return &m_ExSkelPagedIndexBuffer; }
 	RBuffer* GetVobInstanceBuffer(){return m_VobInstanceBuffer;}
 
+	// Returns the atlas for lightmaps of the given size
+	RTextureAtlas* GetLightmapAtlas(const INT2& singleTextureSize);
+
+	// Constructs all lightmap atlases
+	void ConstructLightmapAtlases();
+
+	// Deletes all lightmap atlases
+	void ClearLightmapAtlases();
+
 private:
 	// Paged buffer for ExTVertexStructs and indices
 	RPagedBuffer<ExTVertexStruct> m_ExPagedVertexBuffer;
@@ -30,5 +40,8 @@ private:
 	RPagedBuffer<unsigned int> m_ExPagedIndexBuffer;
 	RPagedBuffer<unsigned int> m_ExSkelPagedIndexBuffer;
 	RBuffer* m_VobInstanceBuffer;
+
+	// Map of single-texture sizes and textureatlases
+	std::map<std::pair<size_t,size_t>, RTextureAtlas*> m_LightmapAtlasCache;
 };
 
