73611c7a8a9c919b91f9e3551b0fdabd969aee07
Andre Taulien <a.taulien@live.de>
2016-01-26 20:29:27 +0100

    attempt at validating the size of the internal classes

---
+++ XGothicRnd/ZenGinValidation.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "ZenGinValidation.h"
+#include "zCVob.h"
+#include "zCVisual.h"
+#include "zCView.h"
+#include "zCMaterial.h"
+#include "zCMeshSoftSkin.h"
+#include "zCModel.h"
+#include "zCModelMeshLib.h"
+#include "zCMorphMesh.h"
+#include "zCParticleFX.h"
+#include "zCProgMeshProto.h"
+#include "zCResource.h"
+#include "zCWorld.h"
+#include "zClassDef.h"
+
+namespace ZenGinValidation
+{
+	/** Validates the size of most ZenGin-Classes */
+	void Validate()
+	{
+		/*CHECK_ZCLASS_SIZE(zCVob);
+		CHECK_ZCLASS_SIZE(zCVisual);
+		CHECK_ZCLASS_SIZE(zCView);
+		CHECK_ZCLASS_SIZE(zCMaterial);
+		CHECK_ZCLASS_SIZE(zCMeshSoftSkin);
+		CHECK_ZCLASS_SIZE(zCModel);
+		CHECK_ZCLASS_SIZE(zCModelMeshLib);
+		CHECK_ZCLASS_SIZE(zCMorphMesh);
+		CHECK_ZCLASS_SIZE(zCParticleFX);
+		CHECK_ZCLASS_SIZE(zCProgMeshProto);
+		CHECK_ZCLASS_SIZE(zCResource);
+		CHECK_ZCLASS_SIZE(zCWorld);*/
+	}
+}
---
+++ XGothicRnd/ZenGinValidation.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace ZenGinValidation
+{
+	/** Validates the size of most ZenGin-Classes */
+	void Validate();
+}
