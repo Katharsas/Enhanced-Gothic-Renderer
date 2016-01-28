987709deb7cbd3313cd1dd00251ee655997e47ce
Andre Taulien <a.taulien@live.de>
2016-01-28 19:50:06 +0100

    removing hooks on application close

--- XGothicRnd/Hooks.cpp
+++ XGothicRnd/Hooks.cpp
@@ -4,11 +4,31 @@
 
 namespace Hooks
 {
+	struct _hkHelper
+	{
+		~_hkHelper()
+		{
+			// Remove all hooks when the program ends or we got unloaded
+			for(auto& p : m_AppliedHooks)
+			{
+				DetourRemove(p.first, p.second);
+			}
+		}
+
+		std::vector<std::pair<PBYTE, PBYTE>> m_AppliedHooks;
+	}hkHelper;
+
 	/**
 	 * Direct hooking-function. Works by passing only memory adresses.
 	 */
 	void* HookDirect(const void* address, const void* hook)
 	{
-		return DetourFunction((PBYTE)address, (PBYTE)hook);
+		PBYTE trampoline = DetourFunction((PBYTE)address, (PBYTE)hook);
+
+		// Store the pointers, so we can remove the hook later
+		if(trampoline)
+			hkHelper.m_AppliedHooks.push_back(std::make_pair(trampoline, (PBYTE)hook));
+
+		return trampoline;
 	}
 };
